ed4138121d1daf22f365134e6cae24a998c05516
yunzae <dsa05037@naver.com>
2023-09-18 15:17:57 +0900

    feat: delete

--- pnutour/lib/sculpture/sculptureInfo.dart
+++ pnutour/lib/sculpture/sculptureInfo.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 
-class scultureSerach extends StatelessWidget {
-  const scultureSerach({Key? key}) : super(key: key);
+class sculptureInfo extends StatelessWidget {
+  final String code;
+  const sculptureInfo({required this.code, Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home:Scaffold(
             appBar: AppBar(
-
-                title: Text(
-                    "조형물",
-                    style: TextStyle(
-                        color: Colors.white,
-                        letterSpacing: 2.0,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                    )
-                ),
+              title: Text(
+                  "조형물 정보",
+                  style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 2.0,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  )
+              ),
               centerTitle: true,
               backgroundColor: Color.fromRGBO(15, 85, 190, 1),
               leading: IconButton(
@@ -27,7 +28,7 @@ class scultureSerach extends StatelessWidget {
                 },
               ),
             ),
-            body: Text("내용")
+            body: Text(code)
         )
     );
   }
